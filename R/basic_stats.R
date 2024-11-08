1c16dad9bf54861ce7b9bc589f4a921b1ebcb8ff
Andrew Fullerton <afully00@gmail.com>
2024-11-08 00:54:57 -0800

    initial build

--- R/basic_stats.R
+++ R/basic_stats.R
@@ -1,3 +1,22 @@
+#' @title Basic summary stats by strata
+#' @description Compute mean, median, range, and sample size by strata.
+#'
+#' @param data a data frame or tibble containing `x` and `by`.
+#' @param x a numeric variable in the dataset to compute summary statistics for.
+#' @param by a categorical variable in the dataset by which to compute summary statistics.
+#' @param na.rm a logical evaluation to TRUE or FALSE indicating whether `NA` values should be stripped.
+#' @param ... for further arguments to modify the calculation and formatting of the summary statistics.
+#'
+#' @return a tibble with mean, median, range, and sample size calculations by strata.
+#' @import dplyr tibble
+#' @importFrom stats median na.omit
+#' @export
+#'
+#' @examples
+#' # Basic usage:
+#' basic_stats(iris, Sepal.Width, Species)
+#' # Advanced usage:
+#' basic_stats(iris, Sepal.Width, Species, na.rm = FALSE, trim = 0.1)
 basic_stats <- function(data, x, by, na.rm = TRUE, ...) {
   # Check if input 'data' is a non-empty data frame or tibble
   if (!is.data.frame(data) || nrow(data) == 0 || ncol(data) == 0) {
@@ -5,7 +24,7 @@ basic_stats <- function(data, x, by, na.rm = TRUE, ...) {
   }
 
   # Convert to tibble for consistency
-  data <- as_tibble(data)
+  data <- tibble::as_tibble(data)
 
   # Store variable names for use later in function
   x_name <- deparse(substitute(x))
@@ -16,24 +35,24 @@ basic_stats <- function(data, x, by, na.rm = TRUE, ...) {
     stop(x_name, " not found in the data.")
   } else if (length(na.omit(data |> pull({{ x }}))) == 0) {
     stop(x_name, " doesn't contain any non-missing values.")
-  } else if (!is.numeric(data |> pull({{ x }}))) {
+  } else if (!is.numeric(data |> dplyr::pull({{ x }}))) {
     stop("Variable ", x_name, " must be numeric.")
   }
 
   # Check if 'by' exists and is usable
   if (!by_name %in% names(data)) {
     stop("Grouping variable ", by_name, " not found in the data.")
-  } else if (length(na.omit(data |> pull({{ by }}))) == 0) {
+  } else if (length(stats::na.omit(data |> dplyr::pull({{ by }}))) == 0) {
     stop("Grouping variable ", by_name, "doesn't contain any non-missing values.")
   }
 
-  by_var <- data |> pull({{ by }}) # extract column from data frame as vector
+  by_var <- data |> dplyr::pull({{ by }}) # extract column from data frame as vector
 
   if (!is.factor(by_var)) {
     unique_count <- length(unique(by_var)) # count unique values in grouping var
 
     if (is.character(by_var) && unique_count < 20) {
-      data <- data |> mutate({{ by }} := as.factor({{ by }})) # convert to factor
+      data <- data |> dplyr::mutate({{ by }} := as.factor({{ by }})) # convert to factor
       warning(by_name, " has been converted to a factor with ", unique_count, " levels.")
     } else {
       stop("Grouping variable must be a non-empty factor or a character variable with fewer than 20 unique values.")
@@ -50,7 +69,7 @@ basic_stats <- function(data, x, by, na.rm = TRUE, ...) {
     group_by({{ by }}) |>
     summarise(
       mean = mean({{ x }}, na.rm = na.rm, ...),
-      median = median({{ x }}, na.rm = na.rm, ...),
+      median = stats::median({{ x }}, na.rm = na.rm, ...),
       range = paste0(min({{ x }}, na.rm = na.rm, ...), " - ",
                      max({{ x }}, na.rm = na.rm, ...)),
       n = n()
---
+++ tests/testthat/test-basic_stats.R
@@ -0,0 +1,33 @@
+# Create test dataset with NAs
+set.seed(12) # set starting point for randomization
+iris_with_na <- iris |>
+  mutate(Sepal.Length = replace(Sepal.Length, sample(n(), 10), NA)) # insert 10 NA values into the Sepal.Length column at random
+
+test_that("mean, median, range, and n are computed and output is structured as intended", {
+  result <- basic_stats(iris_with_na, Sepal.Length, Species)
+
+  expect_s3_class(result, "tbl_df") # tibble output
+  expect_equal(nrow(result), length(levels(iris$Species))) # 1 row per species
+  expect_true(all(c("mean", "median", "range", "n") %in% names(result))) # summary stats as columns
+  expect_false(any(is.na(result))) # no null values made it into the output
+})
+
+# Create test dataset with type incompatibilities
+iris_chr_test <- iris |>
+  mutate(
+    Sepal.Length = as.character(Sepal.Length),
+    Species = as.character(Species)
+  )
+
+iris_num_test <- iris |>
+  mutate(Sepal.Length = as.factor(Sepal.Length))
+
+test_that("variable types are handled appropriately", {
+
+  expect_error(basic_stats(iris_chr_test, Sepal.Width, Sepal.Length),
+               "Grouping variable must be a non-empty factor or a character variable with fewer than 20 unique values.")
+  expect_warning(basic_stats(iris_chr_test, Sepal.Width, Species),
+                 "Species has been converted to a factor with 3 levels.")
+  expect_error(basic_stats(iris_num_test, Sepal.Length, Species),
+               "Variable Sepal.Length must be numeric.")
+})
